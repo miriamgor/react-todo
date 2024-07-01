7f9bf74626ba3f78d6e1b212fc8b6969a0d83064
Miriam Gordillo-Chavez <miriamgordillochavez@gmail.com>
2024-07-01 16:19:55 -0500

    completed  lesson 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
+import { useState } from "react";
 
 function App() {
+
+  const [newTodo, setNewTodo] = useState("");
+
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm />
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p>{newTodo}</p>
       <TodoList />
       <hr />
      
