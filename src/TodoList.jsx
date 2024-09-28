675e112b7bd88f7ddbdd15c68d8cadb7f4601934
Miriam Gordillo-Chavez <miriamgordillochavez@gmail.com>
2024-09-28 09:38:43 -0500

    lesson_3_1

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,21 @@
 import TodoListItem from "./TodoListItem";
+import styles from "./TodoList.module.css";
 
 function TodoList({ todoList, onRemoveTodo }) {
   return (
-    <ul>
-      {todoList.map((item) => {
-        return (
-          <TodoListItem key={item.id} todo={item} onRemoveTodo={onRemoveTodo} />
-        );
-      })}
-    </ul>
+    <div className={styles.todoListDiv}>
+      <ul className={styles.ulElement}>
+        {todoList.map((item) => {
+          return (
+            <TodoListItem
+              key={item.id}
+              todo={item}
+              onRemoveTodo={onRemoveTodo}
+            />
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
